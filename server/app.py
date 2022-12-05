eb95c7512f125d713c5de6da57cbcd6c9aacaa75
Eric Paulsen <ericpaulsen@coder.com>
2022-12-04 18:51:53 -0600

    Revert "feat: flask server"
    This reverts commit a82862c35f8fecbe9258ba8fa7a447951a028a15.

--- server/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, current_app
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return current_app.send_static_file('index.html')
-
-app.run(debug=True,host='0.0.0.0',port=5000)
