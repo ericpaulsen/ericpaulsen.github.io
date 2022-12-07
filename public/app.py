0f2316af35e0c2943eec6cc0a2de7890f29efd9f
Eric Paulsen <ericpaulsen@coder.com>
2022-12-06 19:57:50 -0600

    refactor: flask + firebase

--- public/app.py
+++ public/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, current_app
+from flask import Flask, render_template
 import os
 app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def home():
-    return current_app.send_static_file('index.html')
+    css = 'main.css'
+    return render_template('index.html')
 
 if __name__=='__main__':
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
