ab8e334b79b36ee5d47a458121fd0d7aed6a944a
Eric Paulsen <ericpaulsen@coder.com>
2022-12-06 19:06:10 -0600

    flask: serve static assets

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, current_app
 import os
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def home():
     return current_app.send_static_file('index.html')
 
-app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+if __name__=='__main__':
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
